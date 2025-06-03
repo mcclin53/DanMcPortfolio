a47d8b903ba5992c6961ff7f4b995f9ca46a695e
mcclin53 <dan.mcclintic@gmail.com>
2025-06-02 22:36:13 -0400

    fixed projects and README

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,44 +3,68 @@ export default function Project() {
     <div className="container">
         <ul class="column">
             <li>
-        <a href="https://github.com/mcclin53/Weather-Dashboard" target="_blank" rel="noopener noreferrer">
+        <h2>Chef Buddy</h2>
+        <img src={'/ChefBuddy.png'} alt="Chef Buddy" />
+        <a href="https://github.com/JayC-06/ChefBuddy" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://chefbuddy-as26.onrender.com/" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
+      </li>
+      <li>
+        <h2>Weather Dashboard</h2>
         <img src={'/WeatherDash.jpg'} alt="Weather Dashboard" />
-        <button>Weather Dashboard</button>
-      </a>
-      </li>
-      <li>
-        <a href="https://github.com/mcclin53/Employee-Database-Manager" target="_blank" rel="noopener noreferrer">
-        <img src={'/EmployeeManager.jpg'} alt="Dan's Picture" />
-        <button>Employee Database Manager</button>
-      </a>
+        <a href="https://github.com/mcclin53/Weather-Dashboard" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://weather-dashboard-892o.onrender.com/" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
       </li>
       </ul>
       <ul class="column">
         <li>
-        <a href="https://github.com/mcclin53/Vehicle-Builder" target="_blank" rel="noopener noreferrer">
-        <img src={'/VehicleBuilder.jpg'} alt="Vehicle Builder" />
-        <button>Vehicle Builder</button>
-      </a>
-      </li>
-      <li>
-        <a href="https://github.com/mcclin53/Readmegenerator" target="_blank" rel="noopener noreferrer">
+        <h2>README Generator</h2>
         <img src={'/ReadmeGen.jpg'} alt="Readme Generator" />
-        <button>Readme Generator</button>
-      </a>
+        <a href="https://github.com/mcclin53/Readmegenerator" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://github.com/mcclin53/Readmegenerator/raw/refs/heads/main/utils/ReadmeGeneratorDemo.mp4" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
+      </li>
+      <li>
+        <h2>Resume Pilot</h2>
+        <img src={'/ResumePilot.png'} alt="Resume Pilot" />
+        <a href="https://github.com/Jarred13D/ResumePilot" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://resumepilot-r1lo.onrender.com/" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
       </li>
       </ul>
       <ul class="column">
         <li>
-        <a href="https://github.com/mcclin53/Dan-s-Mini-Project-1" target="_blank" rel="noopener noreferrer">
-        <img src={'/HerbieSite.jpg'} alt="Mini Project" />
-        <button>Mini Project</button>
-      </a>
+        <h2>Thanks But No Thanks</h2>
+        <img src={'/TBNY.jpg'} alt="Thanks But No THanks" />
+        <a href="https://jayc-06.github.io/GIFT-EXCHANGE-/index.html" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://jayc-06.github.io/GIFT-EXCHANGE-/index.html" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
       </li>
       <li>
-        <a href="https://jayc-06.github.io/GIFT-EXCHANGE-/index.html" target="_blank" rel="noopener noreferrer">
-        <img src={'/TBNY.jpg'} alt="Gift Exchange Site" />
-        <button>Gift Exchange</button>
-      </a>
+        <h2>Employee Database Manager</h2>
+        <img src={'/EmployeeManager.jpg'} alt="Employee Manager" />
+        <a href="https://github.com/mcclin53/Employee-Database-Manager" target="_blank" rel="noopener noreferrer">
+        <button>GitHub Link</button>
+        </a>
+        <a href="https://www.youtube.com/watch?v=WabUCbRThXg" target="_blank" rel="noopener noreferrer">
+        <button>Render Link</button>
+        </a>
       </li>
       </ul>
     </div>
